9680dae3640366e30757fba3cf16d544c2cfdf36
Anna <alioubimova@yahoo.com>
2015-11-11 20:05:10 -0500

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ resources :user_sessions, :only => [:new, :create, :destroy]
 
   # You can have the root of your site routed with "root"
    root 'links#index'
-
+   
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
